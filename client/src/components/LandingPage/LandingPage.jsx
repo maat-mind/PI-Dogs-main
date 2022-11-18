5173ae242673bdff53aaa79d940222c1b44fadd0
Oscar Bernal <odbg2013@gmail.com>
2022-11-18 17:54:26 -0500

    add landingPage style

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import DogLandingImage from '../../assets/img/lading-dog-image-transformed.png'
+import './landing-page.css'
 
 export default function LandingPage() {
   return (
-    <div>
-      <h1>Henry Dogs</h1>
-      <Link to='/home'>
-        <button>Ingresar</button>
-      </Link>
-    </div>
+    <>
+      <div className='landing'>
+        <section className='landing-section'>
+          <h1 className='landing-title'>Henry Dogs</h1>
+          <Link to='/home'>
+            <button className='landing-button'>Enter</button>
+          </Link>
+        </section>
+        <aside className='landing-aside'>
+          <img
+            className='landing-image'
+            src={DogLandingImage}
+            alt='dog landing page'
+          />
+        </aside>
+      </div>
+      <footer className='landing-footer'>
+        <p>
+          <a
+            className='landing-github'
+            href='http://github.com/maat-mind'>
+            @maatmind
+          </a>{' '}
+          with 💛
+        </p>
+      </footer>
+    </>
   )
 }
