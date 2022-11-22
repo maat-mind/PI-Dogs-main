e60ec7b3db7185cfc951a5fdf5dc818b3e317cba
Oscar Bernal <odbg2013@gmail.com>
2022-11-22 17:54:08 -0500

    add dogDetail style

--- client/src/components/DetailDog/DetailDog.jsx
+++ client/src/components/DetailDog/DetailDog.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link, useParams } from 'react-router-dom'
 import { getDetail } from '../../redux/actions'
+import './detail-dog.css'
 
 export default function DetailDog() {
   const { id } = useParams()
@@ -13,30 +14,35 @@ export default function DetailDog() {
   }, [id, dispatch])
 
   return (
-    <div>
-      <Link to='/home'>
-        <button>Return</button>
-      </Link>
-      <main>
-        <h1>{dog.name}</h1>
+    <div className='detail-main'>
+      <span className='detail-btn-container'>
+        <Link to='/home'>
+          <button className='detail-btn-return'>Return</button>
+        </Link>
+      </span>
+      <main className='detail-info'>
+        <h1 className='detail-title-name'>{dog.name}</h1>
         <img
+          className='detail-image border'
           src={dog.image}
           alt={dog.name}
-          style={{ width: '500px' }}
         />
-        <p>
-          <b>Height</b>: from {dog.height_min || 'X'} to {dog.height_max || 'X'}{' '}
-          cm
-        </p>
-        <p>
-          <b>Weight</b>: from {dog.weight_min || 'X'} to {dog.weight_max || 'X'}{' '}
-          kg
-        </p>
-        <p>
-          <b>Life time</b>: of {dog.life_span_min || 'X'} to{' '}
-          {dog.life_span_max || 'X'} yrs
-        </p>
-        <p>{dog.temperament}</p>
+        <div className='detail-additional-info'>
+          <p>
+            <b>Height</b>: from {dog.height_min || 'X'} to{' '}
+            {dog.height_max || 'X'} cm
+          </p>
+          <p>
+            <b>Weight</b>: from {dog.weight_min || 'X'} to{' '}
+            {dog.weight_max || 'X'} kg
+          </p>
+          <p>
+            <b>Life time</b>: of {dog.life_span_min || 'X'} to{' '}
+            {dog.life_span_max || 'X'} yrs
+          </p>
+          <br />
+          <p>{dog.temperament}</p>
+        </div>
       </main>
     </div>
   )
